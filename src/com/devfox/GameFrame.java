e957983888499f8c2363bbc8de5c3ffb1998cf09
devfox <devfox3217@gmail.com>
2021-09-26 22:53:22 +0900

    완성

--- src/com/devfox/GameFrame.java
+++ src/com/devfox/GameFrame.java
@@ -4,4 +4,14 @@ import javax.swing.*;
 
 public class GameFrame extends JFrame {
 
+    GameFrame() {
+
+        this.add(new GamePanel());
+        this.setTitle("뱀게임");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+    }
 }
